34b5015f76df6c035ed9453b98da172553672a34
79506 <brusnitsyn.i@gmail.com>
2024-03-14 18:32:26 +0500

    first commit

---
+++ src/main/java/edu/innotech/inno/InnoApplication.java
@@ -0,0 +1,12 @@
+package edu.innotech.inno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InnoApplication.class, args);
+	}
+
+}
